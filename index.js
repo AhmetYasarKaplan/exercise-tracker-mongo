15fb5158434f1dd6ca0ef9708216331988a0ec18
AhmetYasarKaplan <e210503060@stud.tau.edu.tr>
2024-09-13 22:24:08 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const userSchema = new mongoose.Schema({
+  username: String,
+});
+const exerciseSchema = new mongoose.Schema({
+  description: String,
+  duration: Number,
+  date: Date,
+});
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
+app.get("/api/users", async (req, res) => {});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
